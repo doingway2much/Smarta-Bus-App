87158c9dc85572e0e5b188f3d20f923a4c769bdd
Saeed Altaf <saeedaltaf@gmail.com>
2019-07-16 19:25:41 -0400

    Created initial html layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+
+function getBuses() {
+    
+	$.ajax({
+    url: 'http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetAllBus',
+    type: 'GET',
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: function (data) { 
+        $.each(data, function(index, element) {
+            $('body').append($('<div>', {
+                text: element.name
+            }));
+        });
+    }
+});
+}
