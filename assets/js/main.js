1c183946418e46531d501a35bc1ed054d574b35c
Saeed Altaf <saeedaltaf@gmail.com>
2019-07-18 18:41:57 -0400

    Updated with live location

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+// function initMap() {
+//     // The location of Atlanta
+//     var atlanta = { lat: 33.7763658, lng: -84.3899218 };
+//     // The map, centered at Atlanta
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), { zoom: 18, center: atlanta });
+//     // The marker, positioned at Atlanta
+//     var marker = new google.maps.Marker({ position: atlanta, map: map });
+// }
+
 var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
