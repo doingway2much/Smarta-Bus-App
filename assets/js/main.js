a0f0efcead0e8071d39c3cc520c1e3ad4e80ad3b
Saeed Altaf <saeedaltaf@gmail.com>
2019-07-18 18:39:31 -0400

    Updated maps with live location

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,91 @@
-// console.log("Hello World");
-
-// function getBuses() {
-
-// 	$.ajax({
-//     url: 'http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetAllBus',
-//     type: 'GET',
-//     data: { get_param: 'value' }, 
-//     dataType: 'json',
-//     success: function (data) { 
-//         $.each(data, function(index, element) {
-//             $('body').append($('<div>', {
-//                 text: element.name
-//             }));
-//         });
-//     }
-// });
-// }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+var map, infoWindow;
 function initMap() {
-    // The location of Atlanta
-    var atlanta = { lat: 33.7763658, lng: -84.3899218 };
-    // The map, centered at Atlanta
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 18, center: atlanta });
-    // The marker, positioned at Atlanta
-    var marker = new google.maps.Marker({ position: atlanta, map: map });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/
+  //GetAllBus
+  //GetBusByRoute
+  
+  var queryURL = "http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetAllBus";
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+
+
+    
+  function MartaApi (apiKey) {
+    if (!(this instanceof MartaApi)) {
+      return new MartaApi(apiKey)
+    }
+    this.apiKey = apiKey
+  }
+  MartaApi.prototype.getAllRealtimeBusArrivals = function (callback) {
+    return handleCallbacksOrPromises(callback, function (resolve, reject) {
+      request(REALTIME_BUS_ALL_ENDPOINT, function (error, response, body) {
+        if (error) {
+          return reject(error)
+        }
+        var arrivals = JSON.parse(body).map(convertBusArrival)
+        resolve(arrivals)
+      })
+    })
+  }
+  
+  MartaApi.prototype.getRealtimeBusArrivalsByRoute = function (route, callback) {
+    return handleCallbacksOrPromises(callback, function (resolve, reject) {
+      if (route == null) {
+        return reject(new Error('No route supplied'))
+      }
+      request(REALTIME_BUS_ROUTE_ENDPOINT + '/' + route, function (error, response, body) {
+        if (error) {
+          return reject(error)
+        }
+        var arrivals = JSON.parse(body).map(convertBusArrival)
+        resolve(arrivals)
+      })
+    })
+  }
+
+
+
+
+
+
+  
